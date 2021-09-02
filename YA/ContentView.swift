c220d829ab1c6a9f14f4c5e96638ba0b46eb0591
OOOHA <45551120+OOOHA@users.noreply.github.com>
2021-09-02 14:23:43 +0800

    Initial Commit

---
+++ YA/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YA
+//
+//  Created by 邱宏宇 on 2021/9/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YA/YAApp.swift
@@ -0,0 +1,17 @@
+//
+//  YAApp.swift
+//  YA
+//
+//  Created by 邱宏宇 on 2021/9/2.
+//
+
+import SwiftUI
+
+@main
+struct YAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
